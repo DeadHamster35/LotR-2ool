2d0b6f844d06faf9976178acaabe067072eb9633
DeadHamster35 <50299127+DeadHamster35@users.noreply.github.com>
2023-10-23 10:46:24 -0400

    Initial commit

---
+++ LotR 2ool/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace LotR_2ool
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.VariableBox = new System.Windows.Forms.TextBox();
+            this.createOffsetsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.PropListBox = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(324, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.createOffsetsToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // VariableBox
+            // 
+            this.VariableBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.VariableBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VariableBox.Location = new System.Drawing.Point(62, 370);
+            this.VariableBox.Name = "VariableBox";
+            this.VariableBox.Size = new System.Drawing.Size(200, 29);
+            this.VariableBox.TabIndex = 0;
+            this.VariableBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.VariableBox.TextChanged += new System.EventHandler(this.VariableBox_TextChanged);
+            this.VariableBox.KeyUp += new System.Windows.Forms.KeyEventHandler(this.VariableBox_KeyUp);
+            // 
+            // createOffsetsToolStripMenuItem
+            // 
+            this.createOffsetsToolStripMenuItem.Name = "createOffsetsToolStripMenuItem";
+            this.createOffsetsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.createOffsetsToolStripMenuItem.Text = "CreateOffsets";
+            this.createOffsetsToolStripMenuItem.Click += new System.EventHandler(this.createOffsetsToolStripMenuItem_Click);
+            // 
+            // PropListBox
+            // 
+            this.PropListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PropListBox.FormattingEnabled = true;
+            this.PropListBox.Location = new System.Drawing.Point(12, 27);
+            this.PropListBox.Name = "PropListBox";
+            this.PropListBox.Size = new System.Drawing.Size(300, 329);
+            this.PropListBox.TabIndex = 0;
+            this.PropListBox.SelectedIndexChanged += new System.EventHandler(this.PropListBox_SelectedIndexChanged);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 411);
+            this.Controls.Add(this.PropListBox);
+            this.Controls.Add(this.VariableBox);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Main";
+            this.Text = "LotR 2ool";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox VariableBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem createOffsetsToolStripMenuItem;
+        private System.Windows.Forms.ListBox PropListBox;
+    }
+}
+
---
+++ LotR 2ool/Form1.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Globalization;
+
+namespace LotR_2ool
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MapOpen();
+        }
+
+        int[] VariableArray;
+        int[] OffsetArray;
+
+        string ExecutablePath;
+
+
+        private void GetData()
+        {
+            OpenFileDialog FileOpen = new OpenFileDialog();
+            MessageBox.Show("Please select your Lords2 Executable (Lords2.exe)");
+
+            if (!(FileOpen.ShowDialog() == DialogResult.OK))
+            {
+                return;
+            }
+            if (!File.Exists(FileOpen.FileName))
+            {
+                return;
+            }
+
+
+
+            byte[] Data = File.ReadAllBytes(FileOpen.FileName);
+            MemoryStream memoryStream = new MemoryStream();
+            BinaryReader binaryReader = new BinaryReader(memoryStream);
+            memoryStream.Write(Data, 0, Data.Length);
+            List<string> Output = new List<string>();
+
+
+            binaryReader.BaseStream.Position = 0x0D2BB8;
+
+            for (int Pass = 0; Pass < 198; Pass++)
+            {
+                Output.Add(binaryReader.BaseStream.Position.ToString("x") + "_ " + "N/A" + " _" + binaryReader.ReadInt32().ToString());                
+            }
+
+            binaryReader.BaseStream.Position = 0x0D36C4;
+
+            for (int Pass = 0; Pass < 51; Pass++)
+            {
+                Output.Add(binaryReader.BaseStream.Position.ToString("x") + " _ " + "N/A" + " _" + binaryReader.ReadInt32().ToString());
+            }
+
+            binaryReader.BaseStream.Position = 0x0D37E0;
+
+            for (int Pass = 0; Pass < 16; Pass++)
+            {
+                Output.Add(binaryReader.BaseStream.Position.ToString("x") + " _ " + "N/A" + " _" + binaryReader.ReadInt32().ToString());
+            }
+
+            binaryReader.BaseStream.Position = 0x0D4508;
+
+            for (int Pass = 0; Pass < 50; Pass++)
+            {
+                Output.Add(binaryReader.BaseStream.Position.ToString("x") + " _ " + "N/A" + " _" + binaryReader.ReadInt32().ToString());
+            }
+
+            binaryReader.BaseStream.Position = 0x0D6974;
+
+            for (int Pass = 0; Pass < 585; Pass++)
+            {
+                Output.Add(binaryReader.BaseStream.Position.ToString("x") + " _ " + "N/A" + " _" + binaryReader.ReadInt32().ToString());
+            }
+
+            binaryReader.BaseStream.Position = 0x0D9DEC;
+
+            for (int Pass = 0; Pass < 16; Pass++)
+            {
+                Output.Add(binaryReader.BaseStream.Position.ToString("x") + " _ " + "N/A" + " _" + binaryReader.ReadInt32().ToString());
+            }
+
+            binaryReader.BaseStream.Position = 0x0DA270;
+
+            for (int Pass = 0; Pass < 68; Pass++)
+            {
+                Output.Add(binaryReader.BaseStream.Position.ToString("x") + " _ " + "N/A" + " _" + binaryReader.ReadInt32().ToString());
+            }
+
+            binaryReader.BaseStream.Position = 0x0DA3DC;
+
+            for (int Pass = 0; Pass < 44; Pass++)
+            {
+                Output.Add(binaryReader.BaseStream.Position.ToString("x") + " _ " + "N/A" + " _" + binaryReader.ReadInt32().ToString());
+            }
+
+            binaryReader.BaseStream.Position = 0x0DC8F0;
+
+            for (int Pass = 0; Pass < 119; Pass++)
+            {
+                Output.Add(binaryReader.BaseStream.Position.ToString("x") + " _ " + "N/A" + " _" + binaryReader.ReadInt32().ToString());
+            }
+
+            SaveFileDialog FileSave = new SaveFileDialog();
+            if (FileSave.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllLines(FileSave.FileName, Output.ToArray());
+            }
+        }
+        
+
+
+        public void MapOpen()
+        {
+            string PluginPath = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "LotR2ool.plugin");
+            if (!File.Exists(PluginPath))
+            {
+                MessageBox.Show("Missing 'LotR2ool.plugin' file, please repair installation or run from the program directory.");
+                return;
+            }
+
+            OpenFileDialog FileOpen = new OpenFileDialog();
+            MessageBox.Show("Please select your Lords2 Executable (Lords2.exe)");
+
+            if (!(FileOpen.ShowDialog() == DialogResult.OK))
+            {
+                return;
+            }
+            if (!File.Exists(FileOpen.FileName))
+            {
+                return;
+            }
+
+            ExecutablePath = FileOpen.FileName;
+
+            byte[] Data = File.ReadAllBytes(ExecutablePath);
+            MemoryStream memoryStream = new MemoryStream();
+            BinaryReader binaryReader = new BinaryReader(memoryStream);
+            memoryStream.Write(Data, 0, Data.Length);
+            memoryStream.Position = 0;
+
+            string[] PluginData = File.ReadAllLines(PluginPath);
+
+            int PropCount = PluginData.Length;
+            VariableArray = new int[PropCount];
+            OffsetArray = new int[PropCount];
+            PropListBox.Items.Clear();
+            int ParseInt = 0;
+            for (int ThisProp = 0; ThisProp < PropCount; ThisProp++)
+            {
+                string[] Split = PluginData[ThisProp].Split('_');
+
+                if ( (Split.Length < 2) || (!int.TryParse(Split[0], System.Globalization.NumberStyles.HexNumber, CultureInfo.CurrentCulture, out ParseInt )) )
+                {
+                    PropListBox.Items.Add("Void");
+                    VariableArray[ThisProp] = -1;
+                    continue;
+                }
+                PropListBox.Items.Add(Split[1]);
+                OffsetArray[ThisProp] = ParseInt;
+                binaryReader.BaseStream.Position = OffsetArray[ThisProp];
+                VariableArray[ThisProp] = binaryReader.ReadInt32();
+
+            }
+
+
+        }
+
+        private void createOffsetsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GetData();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PropListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (VariableArray.Length > PropListBox.SelectedIndex)
+            {
+                VariableBox.Text = VariableArray[PropListBox.SelectedIndex].ToString();
+            }
+        }
+
+        private void VariableBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void VariableBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            int ParseInt;
+            if (PropListBox.SelectedIndex == -1)
+            {
+                return;
+            }
+            if (PropListBox.SelectedIndex > VariableArray.Length)
+            {
+                return;
+            }
+
+
+            if (int.TryParse(VariableBox.Text, out ParseInt))
+            {
+                VariableArray[PropListBox.SelectedIndex] = ParseInt;
+                return;
+            }
+
+            
+            
+            
+        }
+
+
+        public void SaveData()
+        {
+            MessageBox.Show("Please save your new executable");
+            SaveFileDialog FileSave = new SaveFileDialog();
+            if (!(FileSave.ShowDialog()== DialogResult.OK))
+            {
+                return;
+            }
+
+            byte[] FileData = File.ReadAllBytes(ExecutablePath);
+
+            MemoryStream memoryStream = new MemoryStream();
+            BinaryWriter binaryWriter = new BinaryWriter(memoryStream);
+            memoryStream.Write(FileData, 0, FileData.Length);
+            
+
+            for (int ThisData = 0; ThisData < VariableArray.Length; ThisData++)
+            {
+                binaryWriter.BaseStream.Position = OffsetArray[ThisData];
+                binaryWriter.Write(Convert.ToInt32(VariableArray[ThisData]));
+            }
+
+            File.WriteAllBytes(FileSave.FileName, memoryStream.ToArray());
+
+        }
+
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveData();
+        }
+    }
+}
